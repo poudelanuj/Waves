1ce8b4b3d7f704d9ae0c6b72017468eebda26aa0
Anuj Poudel <poudel.01anuj@gmail.com>
2017-12-31 18:26:15 +0545

    key generation from seed

---
+++ src/com/waveswallet/CryptoCurrency.java
@@ -0,0 +1,36 @@
+package com.waveswallet;
+
+public interface CryptoCurrency {
+	byte[] newSeed(String mnemonic, String passphrase);
+
+    /**
+     * Creates a new private key
+     * @return private key
+     */
+    byte[] newPrivateKey();
+
+    /**
+     * Creates a new private key using the provided seed value
+     * @param seed seed value
+     * @return private key
+     */
+    byte[] newPrivateKey(byte[] seed);
+
+    /**
+     * Creates a new private key of given index using the provided seed value
+     * @param seed seed value
+     * @param index index of private key
+     * @return private key
+     */
+    byte[] newPrivateKey(byte[] seed, int index);
+
+    /**
+     * Returns public key corresponding to the given private key
+     * @param privateKey private key
+     * @return public key
+     */
+    byte[] publicKey(byte[] privateKey);
+
+
+
+}
---
+++ src/com/waveswallet/Waves.java
@@ -0,0 +1,135 @@
+package com.waveswallet;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.text.Normalizer;
+import java.util.Arrays;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.digests.Blake2bDigest;
+import org.bouncycastle.crypto.digests.KeccakDigest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+
+import org.whispersystems.curve25519.java.curve_sigs;
+
+
+public class Waves implements CryptoCurrency{
+	
+	private static final Digest BLAKE2B256 = new Blake2bDigest(256);
+    private static final Digest KECCAK256 = new KeccakDigest(256);
+    private static final Digest SHA256 = new SHA256Digest();
+
+	String seedString;
+
+	@Override
+	public byte[] newSeed(String mnemonic, String passphrase) { //BIP-39 algorithm for random seed
+		mnemonic=Normalizer.normalize(mnemonic	,Normalizer.Form.NFKD);
+		passphrase=Normalizer.normalize(passphrase,Normalizer.Form.NFKD);
+		final char[] chars = mnemonic.toCharArray();
+		byte[] salt = null;
+		SecretKeyFactory secretKeyFactory = null;
+		PBEKeySpec spec=null;
+		
+		try {
+			salt = ("mnemonic" + passphrase).getBytes("UTF-8");
+			spec= new PBEKeySpec(chars, salt, 2048, 512);
+			secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			return secretKeyFactory.generateSecret(spec).getEncoded();
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+		
+		
+	}
+
+	@Override
+	public byte[] newPrivateKey() {
+		MessageDigest messagedigest=null;
+		try {
+			messagedigest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		messagedigest.reset();
+		return messagedigest.digest();
+		
+	}
+
+	@Override
+	public byte[] newPrivateKey(byte[] seed) {
+		// account seed from seed & nonce
+        ByteBuffer buf = ByteBuffer.allocate(seed.length + 4);
+        byte[] accountSeed = secureHash(buf.array(), 0, buf.array().length);
+
+        // private key from account seed & scheme
+        byte[] hashedSeed = hash(accountSeed, 0, accountSeed.length, SHA256);
+        byte[] privateKey = Arrays.copyOf(hashedSeed, 32);
+        privateKey[0]  &= 248;
+        privateKey[31] &= 127;
+        privateKey[31] |= 64;
+
+        return privateKey;
+		
+		
+		
+	}
+
+	@Override
+	public byte[] newPrivateKey(byte[] seed, int index) {
+		ByteBuffer buf = ByteBuffer.allocate(seed.length + 4);
+        buf.putInt(index).put(seed);
+        byte[] accountSeed = secureHash(buf.array(), 0, buf.array().length);
+
+        // private key from account seed & scheme
+        byte[] hashedSeed = hash(accountSeed, 0, accountSeed.length, SHA256);
+        byte[] privateKey = Arrays.copyOf(hashedSeed, 32);
+        privateKey[0]  &= 248;
+        privateKey[31] &= 127;
+        privateKey[31] |= 64;
+
+        return privateKey;
+		
+	}
+
+	@Override
+	public byte[] publicKey(byte[] privateKey) {
+		byte[] publicKey = new byte[32];
+        curve_sigs.curve25519_keygen(publicKey, privateKey); 
+        return publicKey;
+	}
+	
+	
+	static byte[] secureHash(byte[] message, int ofs, int len) {
+        byte[] blake2b = hash(message, ofs, len, BLAKE2B256);
+        return hash(blake2b, 0, blake2b.length, KECCAK256);
+	}
+	
+	static byte[] hash(byte[] message, int ofs, int len, Digest alg) {
+        byte[] res = new byte[alg.getDigestSize()];
+        alg.update(message, ofs, len);
+        alg.doFinal(res, 0);
+        return res;
+}
+	
+	
+
+}
